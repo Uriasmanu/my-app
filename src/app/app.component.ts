e549ba19a6aee1dedb7d0a620f15af569d1d273c
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-06-11 13:04:37 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Produto } from './modelo/produto';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +8,20 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
+  http = inject(HttpClient);
+  url = 'https://myloja-api.azurewebsites.net';
+  produtos: Produto[] = [];
+
+  ngOnInit(): void {
+    this.obterProdutos();
+  }
+
+  obterProdutos(){
+    this.http.get<Produto[]>(`${this.url}/estoque`)
+      .subscribe(produtos => this.produtos = produtos)
+  }
 }
